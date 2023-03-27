941f1726b4b7fcd6b4aff8e9f2a9d8aa686fd23d
Ajay Thakur <ajaythk.94@gmail.com>
2023-03-27 22:11:34 +0545

    model creation done

---
+++ quiz/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from .models import Category, Quizzes, Answer, Question
+
+
+# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ["id", "title"]
+
+
+class AnswerInlineModel(admin.TabularInline):
+    model = Answer
+    fields = ['answer_text', 'is_right']
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["title", "quiz"]
+    list_display = ["title", "quiz", 'date_created']
+    inlines = [AnswerInlineModel]
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ["answer_text", "is_right", "question", ]
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Quizzes, QuizAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
---
+++ quiz/models.py
@@ -0,0 +1,86 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(max_length=200, unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Quizzes(models.Model):
+    category = models.ForeignKey(Category, default=1, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        verbose_name = _("Quiz")
+        verbose_name_plural = _("Quizzes")
+        ordering = ["id"]
+
+    title = models.CharField(max_length=255, default=_("New Quiz"), verbose_name=_("Quiz Title"))
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Updated(models.Model):
+    date_updated = models.DateTimeField(verbose_name=_("Last Updated"), auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+# Abstract Base Class is used when same information is used in multiple table.
+
+class Question(Updated):
+
+    class Meta:
+        verbose_name = _("Question")
+        verbose_name_plural = _("Questions")
+        ordering = ["id"]
+
+    SCALE = (
+        (0, _("Fundamental")),
+        (1, _("Beginner")),
+        (2, _("Intermediate")),
+        (3, _("Advanced")),
+        (4, _("Expert"))
+    )
+
+    TYPE = (
+        (0, _("Multiple Choice")),
+
+    )
+
+    quiz = models.ForeignKey(Quizzes, related_name='question', on_delete=models.DO_NOTHING)
+    technique = models.IntegerField(choices=TYPE, default=0, verbose_name=_("Type of Question"))
+    title = models.CharField(max_length=255, verbose_name=_("Title"))
+    difficulty = models.IntegerField(choices=SCALE, default=0, verbose_name=_("Difficulty"))
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name=_("Date Created"))
+    is_active = models.BooleanField(default=False, verbose_name=_("Active Status"))
+
+    def __str__(self):
+        return self.title
+
+
+class Answer(Updated):
+
+    class Meta:
+        verbose_name = _("Answer")
+        verbose_name_plural = _("Answers")
+        ordering = ["id"]
+
+    question = models.ForeignKey(Question, related_name="answer", on_delete=models.DO_NOTHING)
+    answer_text = models.CharField(max_length=255, verbose_name=_("Answer Text"))
+    is_right = models.BooleanField(default=False)
